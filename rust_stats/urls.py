ffe3572fc2095527023efc8b2a99124a6aeb534b
A-GK <58001898+A-GK@users.noreply.github.com>
2020-07-01 21:43:39 -0400

    Add redirect url to invite the bot

--- rust_stats/urls.py
+++ rust_stats/urls.py
@@ -5,7 +5,7 @@ from django.contrib.sitemaps import views as sitemaps_views
 from .sitemap import *
 
 from . import views
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -23,6 +23,7 @@ urlpatterns = [
     path('rust-stats/ban-user', views.ban_user, name='ban_user'),
     path('rust-stats/delete-user', views.delete_user, name='delete_user'),
     path('discord-bot', views.discord_bot_page, name='discord_bot_page'),
+    path('invite-bot', RedirectView.as_view(url='https://discord.com/oauth2/authorize?client_id=727206959287107586&scope=bot&permissions=27712'), name='invite_bot'),
     # 4 hours cache time for sitemaps
     path('sitemap.xml',  cache_page(14400)(sitemaps_views.index), {'sitemaps': sitemaps, 'sitemap_url_name': 'sitemaps'}),
     path('sitemap-<section>.xml', cache_page(14400)(sitemaps_views.sitemap), {'sitemaps': sitemaps}, name='sitemaps'),
