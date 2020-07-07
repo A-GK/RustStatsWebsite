e82af985e82dd16fa9697060f33b3b7a8cb1484b
A-GK <58001898+A-GK@users.noreply.github.com>
2020-07-07 00:51:38 -0400

    Change bot invite link permissions

--- rust_stats/urls.py
+++ rust_stats/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('rust-stats/ban-user', views.ban_user, name='ban_user'),
     path('rust-stats/delete-user', views.delete_user, name='delete_user'),
     path('discord-bot', views.discord_bot_page, name='discord_bot_page'),
-    path('invite-bot', RedirectView.as_view(url='https://discord.com/oauth2/authorize?client_id=727206959287107586&scope=bot&permissions=27712'), name='invite_bot'),
+    path('invite-bot', RedirectView.as_view(url='https://discord.com/oauth2/authorize?client_id=727206959287107586&scope=bot&permissions=92240'), name='invite_bot'),
     # 4 hours cache time for sitemaps
     path('sitemap.xml',  cache_page(14400)(sitemaps_views.index), {'sitemaps': sitemaps, 'sitemap_url_name': 'sitemaps'}),
     path('sitemap-<section>.xml', cache_page(14400)(sitemaps_views.sitemap), {'sitemaps': sitemaps}, name='sitemaps'),
